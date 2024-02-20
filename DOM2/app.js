1ef4b914fcac179265e1a4aef5e4504520873285
Vikky kumar singh <108605520+Vikky2000@users.noreply.github.com>
2024-02-20 12:13:35 +0530

    Add files via upload

---
+++ DOM2/app.js
@@ -0,0 +1,68 @@
+//////////////Attributes /////////////
+////getAttribute(attr)//////
+//let div = document.querySelector("div");
+//console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name= div.getAttribute("name");
+// console.log(name);
+
+//////////setAttribute(attr,value)///////
+//let para = document.querySelector("p");
+//console.log(para.setAttribute("class","newClass"));
+
+//  para.setAttribute("class","123");
+
+
+
+
+///////style////////////
+// let div = document.querySelector("div");
+//console.log(div);
+//div.style.backgroundColor ="red";
+
+
+
+// Insert Elements ///
+// Node.appent(el)// adds at the end of node(inside)
+// Node.prepend(el)// adds at the start of node *(inside)
+
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText= "click me";
+// console.log(newBtn);
+
+
+
+// let div =document.querySelector("div");
+// // div.append(newBtn);
+// div.prepend(newBtn);
+
+// let newHeading = document.createElement("h1");
+// newHeading.innerHTML = "<i>Hi, I am new! </i>";
+
+// document.querySelector("body").prepend(newHeading);
+
+// // delete element//
+
+// let para = document.querySelector("p");
+// para.remove();
+
+// newHeading.remove();
+
+
+
+
+////////////////practice /////////////
+// let newNam = document.createElement("button");
+// newNam.innerText = "click me!";
+
+// newNam.style.color = "white";
+// newNam.style.backgroundColor = "red";
+
+// document.querySelector("body").prepend(newNam);
+
+
+
