c602173f3039c3cb21560d6ef82236ce929bead6
Vikky kumar singh <108605520+Vikky2000@users.noreply.github.com>
2024-02-20 12:12:59 +0530

    Add files via upload

---
+++ DOM/app.js
@@ -0,0 +1,28 @@
+//console.log("hello");
+//window.alert("hello");
+
+
+// <style>   <script>   readability, modular, browser cashing
+
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+
+// let headings = document.getElementsByClassName("heading");
+// console.dir(headings);
+// console.log(headings);
+
+
+// let paragraph =document.getElementsByTagName("p")
+// console.dir(paragraph)
+//console.log("hello");
+
+
+
+// dynamic change with the web page is the important for DOM
+
+let Element = document.querySelector("p");
+console.dir(Element);
+
+
+let Element2 = document.querySelectorAll("p");
+console.dir(Element2);
